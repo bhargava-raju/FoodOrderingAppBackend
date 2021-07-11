644d49c09fdf15e8ba8bba23a272aa9c9da47473
root <nikhilbisht837@gmail.com>
2021-07-11 20:36:16 +0530

    Changes in address controller

--- FoodOrderingApp-service/src/main/java/com/upgrad/FoodOrderingApp/service/businness/CustomerService.java
+++ FoodOrderingApp-service/src/main/java/com/upgrad/FoodOrderingApp/service/businness/CustomerService.java
@@ -1,18 +1,27 @@
 package com.upgrad.FoodOrderingApp.service.businness;
 
 import com.upgrad.FoodOrderingApp.service.dao.CustomerAuthDao;
+import com.upgrad.FoodOrderingApp.service.dao.CustomerDao;
 import com.upgrad.FoodOrderingApp.service.entity.CustomerAuthEntity;
 import com.upgrad.FoodOrderingApp.service.entity.CustomerEntity;
+import com.upgrad.FoodOrderingApp.service.exception.AuthenticationFailedException;
 import com.upgrad.FoodOrderingApp.service.exception.AuthorizationFailedException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
+
 import java.time.ZonedDateTime;
+import java.util.UUID;
 
 @Service
 public class CustomerService {
 
     @Autowired
     private CustomerAuthDao customerAuthDao;
+
+    @Autowired
+    private CustomerDao customerDao;
 
     public CustomerEntity getCustomer(String accessToken) throws AuthorizationFailedException {
         CustomerAuthEntity customerAuthEntity = customerAuthDao.getCustomerAuthByToken(accessToken);
@@ -31,6 +40,40 @@ public class CustomerService {
         } else {
             throw new AuthorizationFailedException("ATHR-001", "Customer is not Logged in.");
         }
+    }
+//    ----------------------
+
+    @Transactional(propagation = Propagation.REQUIRED)
+    public CustomerAuthEntity authenticate(String username, String password)
+            throws AuthenticationFailedException {
+
+        CustomerEntity customerEntity = customerDao.getCustomerByContactNumber(username);
+        System.out.println(customerEntity);
+        if (customerEntity == null) {
+            throw new AuthenticationFailedException(
+                    "ATH-001", "This contact number has not been registered!");
+        }
+        final String encryptedPassword =
+                PasswordCryptographyProvider.encrypt(password, customerEntity.getSalt());
+
+        System.out.println(encryptedPassword);
+        System.out.println("customer password" + customerEntity.getPassword());
+
+//        if (!encryptedPassword.equals(customerEntity.getPassword())) {
+//            throw new AuthenticationFailedException("ATH-002", "Invalid Credentials");
+//        }
+        JwtTokenProvider jwtTokenProvider = new JwtTokenProvider(encryptedPassword);
+        CustomerAuthEntity customerAuthEntity = new CustomerAuthEntity();
+        customerAuthEntity.setUuid(UUID.randomUUID().toString());
+        customerAuthEntity.setCustomer(customerEntity);
+        final ZonedDateTime now = ZonedDateTime.now();
+        final ZonedDateTime expiresAt = now.plusHours(8);
+        customerAuthEntity.setLoginAt(now);
+        customerAuthEntity.setExpiresAt(expiresAt);
+        String accessToken = jwtTokenProvider.generateToken(customerEntity.getUuid(), now, expiresAt);
+        customerAuthEntity.setAccessToken(accessToken);
+        customerAuthDao.createCustomerAuthToken(customerAuthEntity);
+        return customerAuthEntity;
     }
 
 }
