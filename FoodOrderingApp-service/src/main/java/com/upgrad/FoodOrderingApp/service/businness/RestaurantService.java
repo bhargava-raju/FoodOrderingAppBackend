c24c0118b5f39bc5be806d1bcbff5e6b8ff11d57
Bhargava Raju <BhargavaR@anthology.com>
2021-07-15 19:16:14 +0530

    Adding the service methods

--- FoodOrderingApp-service/src/main/java/com/upgrad/FoodOrderingApp/service/businness/RestaurantService.java
+++ FoodOrderingApp-service/src/main/java/com/upgrad/FoodOrderingApp/service/businness/RestaurantService.java
@@ -1,4 +1,27 @@
 package com.upgrad.FoodOrderingApp.service.businness;
 
+import com.upgrad.FoodOrderingApp.service.entity.RestaurantEntity;
+
+import java.util.List;
+
 public class RestaurantService {
+    public RestaurantEntity restaurantByUUID(String uuid) {
+        return null;
+    }
+
+    public List<RestaurantEntity> restaurantsByName(String uuid) {
+        return null;
+    }
+    public String restaurantByCategory(String uuid) {
+        return "";
+    }
+    public String restaurantsByRating(String uuid) {
+        return "";
+    }
+
+    public RestaurantEntity updateRestaurantRating(RestaurantEntity restaurantEntity, Double rating){
+        return null;
+    }
+
+
 }
